1b5ac3897f20b923c9974943001119c556d7f96f
Saqlain Ali Javed <saqlain.pucit@gmail.com>
2024-09-06 20:24:02 +0500

    $node ace make:model Todo -> make model and add same columns as in migration in it.

---
+++ app/Models/Todo.ts
@@ -0,0 +1,26 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Todo extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+
+  @column({
+    serialize: (value?: Number) => Boolean(value),
+  })
+  public status: boolean
+
+  @column()
+  public title: string
+
+  @column()
+  public description: string
+
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
