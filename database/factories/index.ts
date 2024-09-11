48f396d30e5c4d43d5ddaaa931386fc35dca5d40
Saqlain Ali Javed <saqlain.pucit@gmail.com>
2024-09-11 12:51:05 +0500

    add notes

--- database/factories/index.ts
+++ database/factories/index.ts
@@ -1,1 +1,5 @@
 // import Factory from '@ioc:Adonis/Lucid/Factory'
+
+
+// it is used to create mock data in bulk, usully it it used in development to add testing mock data 
+// for front-end. This is not actual data is is fake data, using seeder we add actual data.
